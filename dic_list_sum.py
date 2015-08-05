84fc09a2b9d46311c1bf4c3028266641c9b2dd3a
Starbuff <maria.kareliusson@gmail.com>
2015-08-05 15:35:02 +0200

    update

--- dic_list_sum.py
+++ dic_list_sum.py
@@ -1,3 +1,4 @@
 robot = {"price": 900, "count":2, "vat": 1.25}
 book = {"price": 100, "count": 1, "vat": 1.06}
 print robot["price"] * robot["count"] * robot["vat"] + book["price"] * book["count"] * book["vat"]
+# { " strängar " } som jag lägger i en [ behållare ]
